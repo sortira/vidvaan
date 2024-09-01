94dad6acbc47c821ae95e1cb181be35be501eaea
asif <asiftanvir2006@gmail.com>
2024-09-01 07:23:37 +0530

    excel download fix

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ async function work() {
         for (let fetchPromise of fetchFunctions) {
             const results = await fetchPromise;
             publications.push(...results);
+            originalPublications.push(...results);
             // Display partial results
             displayPublications(publications);
         }
@@ -380,11 +381,45 @@ function displayPublications(publications, currentPage = 1, rowsPerPage = 50) {
 
 
 function exportExcel() {
-    $('.pubs').table2excel({
+    /*
+    $('.sortable').table2excel({
         exclude: ".no-export",
-        filename: "Vidvaan Report on " + document.getElementById("topic").value.trim() + ".xls",
+        filename: "download.xls",
         fileext: ".xls",
         exclude_links: false,
         exclude_inputs: true
     });
+    */
+    
+    
+    //console.log(pubarr);
+    //console.log(originalPublications);
+    const pubarr = originalPublications.map((obj)=>{return Object.keys(obj).map((key)=>{return obj[key];});});
+    var wb = XLSX.utils.book_new();
+    wb.Props
+    {
+        Title:"Vidvaan Report";
+        Subject:"Report";
+        Author:"Team Vidvaan";
+        
+    }
+    wb.SheetNames.push("Report Sheet");
+    pubarr.unshift(["Name     ","Year    ","Authors        ","Link             ","Academic Database"])
+    wb.Sheets["Report Sheet"] = XLSX.utils.aoa_to_sheet(pubarr);
+    var wbout = XLSX.write(wb,{booktype:'xlsx',type:'binary'});
+    saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), 'Vidvaan Report.xlsx');
+
+}
+function convertToArray(tup)
+{
+    var Arr = Object.keys(tup).map(
+        (key) => tup[key]);
+    return Arr;
+}
+
+function s2ab(s) { 
+    var buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
+    var view = new Uint8Array(buf);  //create uint8array as viewer
+    for (var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF; //convert to octet
+    return buf;    
 }
