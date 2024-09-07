c6567e28837b13fe2f2416c0fa17fad1bc9e2ffe
sortira <aritroshomework@gmail.com>
2024-09-07 16:23:26 +0530

    added chatbot functionality

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ with open(file_path, 'r') as file:
 
 
 genai.configure(api_key=API_KEY)
-
+model = genai.GenerativeModel("gemini-1.5-flash")
 app = Flask(__name__)
 CORS(app)
 
@@ -26,12 +26,25 @@ CORS(app)
 def summarise():
     data = request.get_json()
     text_to_summarize = " ".join(data['summaries'])
-    model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content("Write a summary in 250 words about the following titles of research papers in english : " + text_to_summarize)
+    response = model.generate_content("Write a summary (do not use markdown) in 250 words about the following titles of research papers in english : " + text_to_summarize)
     data = {
         'summary' : response.text
     }
     wbresponse = make_response(jsonify(data))
     return wbresponse
+
+@app.route("/chat", methods=['GET'])
+def chat():
+    message = request.args.get('message', '')
+    if not message:
+        return jsonify({'reply': 'No message provided'}), 400
+    response = model.generate_content("Reply formally and concisely to following message (do not reply using markdown) :  " + message)
+    data = {
+        'reply': response.text  # Ensure the GenerativeModel response has a `text` attribute
+    }
+    return jsonify(data)
+
+
+
 if __name__ == "__main__":
     app.run(debug=False)
